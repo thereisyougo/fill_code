3fdf741784660bb18f7f4455c86f908631d5e658
thereisyougo <xper119@live.com>
2019-04-10 00:04:04 +0800

    mod oa

--- js/content.js
+++ js/content.js
@@ -93,7 +93,7 @@ function hfmail() {
 function ydlogin() {
 	if (!document.forms[0].login_username) return;
 	document.forms[0].login_username.value = 'xiaom.liu';
-	document.forms[0].login_password.value = 'Summon#{%718%}';
+	document.forms[0].login_password.value = 'Summon#{%719%}';
 	document.forms[0]['login.VerifyCode'].select();
 	document.forms[0]['login.VerifyCode'].focus();
 	function subform(e) {
